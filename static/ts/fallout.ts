5053da6f9697ff0769620d67974e53d4202c1c79
TJ <tedwardj11@gmail.com>
2023-11-06 17:47:12 -0500

    pin. refactored isSet methods

--- static/ts/fallout.ts
+++ static/ts/fallout.ts
@@ -320,163 +320,56 @@ function getNested(obj: {[key: string]: any}, keys: string[]) {
     let key: string = keys.shift() as string;
     let val = obj[key];
     if (keys.length > 0) {
-        val = getNested(val, keys);
-    }
-    return val;
-}
-
-function getRequired(obj: {[key: string]: any}, key: string): any {
-    let val = obj[key];
-    if (!_isDefinedAndNotNull(val)) {
-        __error();
-    }
-    return val;
-}
-
-function getNestedRequired(obj: {[key: string]: any}, keys: string[]) {
-    let val = getNested(obj, keys);
-    if (!_isDefinedAndNotNull(val)) {
-        __error();
-    }
-    return val;
-}
-
-
-//this method signature should accept EITHER any number of string rest arguments, or exactly one
-//string array. to test: 1, with multiple string args, 2 with one string array arg, 3 with multiple
-//string array args (this one should fail)
-function get2___(obj: {[key: string]: any}, ...keys: (string | string[])[]): any {
-    if (_isDefinedAndNotNull(keys) && isPopulated(keys)) {
-        //Ideally based on the method signature and this conditional check, keys is guaranteed to 
-        //be an array of strings (if we're here). make sure to confirm / test this
-        let key = keys.shift();
-        if (Array.isArray(key)) {
-            //Handle case where ...keys is exactly one array of strings
-            keys = key;
-            key = keys.shift();
-        }
-        if (_isDefinedAndNotNull(obj)) {
-            let val = get2(obj, keys as string[]);
-            if (_isDefinedAndNotNull(val) && typeof(val) == "object" && isPopulated(val)) {
-                val = get2(val, keys as string[])
-            }
-            return val;
-        }
-    } else {
-        __error_console("Invalid input ...keys");
-    }
-    // return "test";
-}
-
-// function get2(obj: {[key: string]: any}, ...keys: string[]): any;
-// function get2(obj: {[key: string]: any}, ...test: string[]): any {
-//   if (keys.length > 0) {
-//     // You can now access 'keys' as an array of string parameters
-//     // Your code here
-//   }
-
-//   return "test";
-// }
-
-
-
-
-
-
-function get_____(obj: {[key: string]: any}): object;
-function get_____(obj: {[key: string]: any}, keys: string[]): any;
-function get_____(obj: {[key: string]: any}, ...keys: string[]): any;
-function get_____(obj: {[key: string]: any}, ...keys: string[]|string[][]): any {
-
-    //TODO: What if input arg keys is array of arrays (because i believe this will be allowed based
-    //on the ...keys: string[]|string[][] def, even though thats not what i want).  Handle.
-    //Basically, 'keys' should ONLY ever be an array of strings, whether this comes from multple
-    //strings via ...keys or from one string array passed in via keys
-    //I think with the check for target key != undefined and not an array handles this edge case,
-    //but need gracefully return something useful to the user - maybe throw exception 
-
-    let targetVal;
-    if (_isDefinedAndNotNull(keys) && isPopulated(keys)) {
-        const targetKey = keys.shift();
-        if (typeof(targetKey) != "undefined" && !Array.isArray(targetKey)) {
-            //todo: revisit this logic - why checking for if key is not array?
-            //see comment above also. this algorithm needs revisited
-            
-            targetVal = obj[targetKey];
-            
-            //Shift the first key off the path; create a new array of the provided keys minus the one
-            //that was shifted off. If there is at least one key remaining, recursively call this
-            //function using the retrieved targetVal & shifted keys array
-            // let keysShifted = [] as string[];
-            // keys.forEach((key) => { keysShifted.push(key)} );
-
-            if (isPopulated(keys)) {
-                get(targetVal, keys)
-            }
-                        
+        if (isSet(val)) {
+            val = getNested(val, keys);
         } else {
-            //throw excpetion here?
+            //If any of the parent nested objects are undefined or null, error out
+            __error();
         }
-
-    } else {
-        targetVal = obj;
     }
+    return val;
+}
 
-    return targetVal;
+function getIfSet(obj: {[key: string]: any}, key: string): any {
+    let val = obj[key];
+    if (!isSet(val)) {
+        __error();
+    }
+    return val;
+}
 
+function getNestedIfSet(obj: {[key: string]: any}, keys: string[]) {
+    let val = getNested(obj, keys);
+    if (!isSet(val)) {
+        __error();
+    }
+    return val;
+}
+
+function isSet(obj: any): boolean;
+function isSet(obj: any, key?: string): boolean {
+    if (_isDefinedAndNotNull(key)) {
+        obj = obj[key as string];
+    }
+    return _isDefinedAndNotNull(obj);
+}
+
+function isSetNested(obj: any, keys: string[]): boolean {
+    let key = keys.shift() as string;
+    let val = obj[key];
+    let targetFieldIsSet = _isDefinedAndNotNull(val);
+    if (keys.length > 0) {
+        if (targetFieldIsSet) {
+            targetFieldIsSet = isSetNested(val, keys);
+        }
+    }
+    return targetFieldIsSet;
 }
 
 
-/**
- * Note: This won't reliably handle objects with overridden 'null' or 'undefined' properties
- * @date 10/22/2023 - 8:40:36 AM
- *
- * @param {*} val
- * @returns {boolean}
- */
-function isSet(val: any): boolean;
-function isSet(obj: object, keys: string[]): boolean;
-function isSet(obj: object, ...keys: string[]): boolean;
-function isSet(valOrObj: any, ...keys: string[]|string[][]): boolean {
 
-    //TODO: What if input arg keys is array of arrays (because i believe this will be allowed based
-    //on the ...keys: string[]|string[][] def, even though thats not what i want).  Handle.
-    //Basically, 'keys' should ONLY ever be an array of strings, whether this comes from multple
-    //strings via ...keys or from one string array passed in via keys
-    //I think with the check for target key != undefined and not an array handles this edge case,
-    //but need gracefully return something useful to the user - maybe throw exception 
-    
-    //First check the provided entity to determine if it is undefined/null. If so, we're done and
-    //can return isSet (which would currently be false)
-    //We also check the length of the 'keys' array; if it is empty then we know we've evaluated out
-    //target value. If it is not empty then we need to continue traversing down the input object,
-    //and will do so by shifting the keys array and recursively calling this method on the new obj
-    let targetIsSet = _isDefinedAndNotNull(valOrObj);
-    if (targetIsSet && (_isDefinedAndNotNull(keys) && isPopulated(keys))) {
 
-        //Get the value at the first key in the key path
-        let targetVal;
-        const targetKey = keys.shift();
-        if (typeof(targetKey) != "undefined" && !Array.isArray(targetKey)) {
-            
-            targetVal = valOrObj[targetKey];
-        
-            //Shift the first key off the path; create a new array of the provided keys minus the one
-            //that was shifted off. If there is at least one key remaining, recursively call this
-            //function using the retrieved targetVal & shifted keys array
-            // let keysShifted = [] as string[];
-            // keys.forEach((key) => { keysShifted.push(key)} );
-            if (isPopulated(keys)) {
-                isSet(targetVal, keys)
-            }
 
-        }
-
-    }
-
-    return targetIsSet;
-
-}
 
 function _isDefinedAndNotNull(val: any): boolean {
     //Strict equality checks for input is UNDEFINED or NULL
